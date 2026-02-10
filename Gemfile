461efce07dd4229cef73d59e4eca1006ec700328
Manu <mac-manu@users.noreply.github.com>
2026-02-10 12:01:34 +0100

    update rails

--- Gemfile
+++ Gemfile
@@ -6,9 +6,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.3.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 6.1.7'
+gem 'rails', '~> 8.1.2'
 # Use Puma as the app server
-gem 'puma', '~> 5.6'
+gem 'puma', '~> 7.2.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootstrap', '~> 4.6.2'
 gem 'excon'
 
-gem 'dotenv-rails', groups: [:development, :test]
+gem 'dotenv-rails', groups: %i[development test]
 
 gem 'rubocop', require: false
 # Reduces boot times through caching; required in config/boot.rb
@@ -37,7 +37,7 @@ end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'listen', '>= 3.10.0', '< 3.11'
   gem 'web-console', '>= 3.3.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -4,36 +4,47 @@ class TravelController < ApplicationController
 
   def search
     countries = find_country(params[:country])
-    unless countries
+    if countries.is_a?(Hash) && countries['error']
+      flash[:alert] = countries['error']
+      return render action: :index
+    end
+    unless countries && countries.is_a?(Array)
       flash[:alert] = 'Country not found'
       return render action: :index
     end
-    puts @country = countries.first
-    #puts @weather = find_weather(@country['capital'], @country['alpha2Code'])
+    @country = countries.first
+    # puts @weather = find_weather(@country['capital'], @country['alpha2Code'])
   end
 
   def find_weather(city, country_code)
-    query = URI.encode("#{city},#{country_code}")
+    query = URI.encode_www_form_component("#{city},#{country_code}")
     request_api(
       "https://community-open-weather-map.p.rapidapi.com/forecast?q=#{query}"
     )
   end
 
   def request_api(url)
+    api_key = ENV['RAPIDAPI_API_KEY']
+    return { 'error' => 'Falta la variable de entorno RAPIDAPI_API_KEY. Por favor, configúrala.' } unless api_key
+
     response = Excon.get(
       url,
       headers: {
         'X-RapidAPI-Host' => URI.parse(url).host,
-        'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+        'X-RapidAPI-Key' => api_key
       }
     )
+    if [401, 403].include?(response.status)
+      return { 'error' => 'Clave de API inválida o sin permisos. Verifica tu RAPIDAPI_API_KEY.' }
+    end
     return nil if response.status != 200
+
     JSON.parse(response.body)
   end
+
   def find_country(name)
     request_api(
-      "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+      "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode_www_form_component(name)}"
     )
   end
-
 end
